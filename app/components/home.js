88cd194fe695398266093d507f5e94102d22d11a
Leah Gerald <Leanator13@gmail.com>
2017-12-06 08:36:26 -0500

    links n navbar

--- app/components/home.js
+++ app/components/home.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
 export default function home (props) {
 
     return (
+    <div>
         <h1>Kitten Academy</h1>
+        <Link className="btn btn-primary btn-block" to="/campus">Our Campuses</Link>
+        <br/>
+        <Link className="btn btn-primary btn-block" to="/students">Our Students</Link>
+    </div>
     )
 }
