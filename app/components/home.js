dc4ded5e34f71919f7580f0a9abaa9724e17326a
Leah Gerald <Leanator13@gmail.com>
2017-12-06 21:30:53 -0500

    form still not working but i tried

--- app/components/home.js
+++ app/components/home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import EditStudents from './EditStudents'
+
+
 export default function home (props) {
 
     return (
     <div>
         <h1>Kitten Academy</h1>
+        <EditStudents />
+
     </div>
 
     )
